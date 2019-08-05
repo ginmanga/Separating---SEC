0ce11dae23427b068fd05369605b57ac7d7899ef
ginmanga <paboenhj@outlook.com>
2019-08-05 08:49:41 +0800

    "Changing COmpstat match to CR"

--- Act_research.py
+++ Act_research.py
@@ -1,1 +1,20 @@
 #here we start the actual reserarch with matched file
+
+import datetime, csv
+import pandas as pd
+import pandasql as ps
+import numpy as np
+AA_FINAL = 'C:/Users/Panqiao/Documents/Research/SS - All/FINAL/final_match.txt'
+CT = 'C:/Users/Panqiao/Documents/Research/SS - All/COMPSTAT/ccmxpf_linktable.sas7bdat'
+CT1 = 'C:/Users/Panqiao/Documents/Research/SS - All/COMPSTAT/ccmxpf_linktable.txt'
+AA_FINAL = pd.read_csv(AA_FINAL, sep=",")
+
+def sas_to_dataframe(file_path):
+    """
+    Read in a SAS dataset (sas7bdat) with the proper encoding
+    :param file_path: location of SAS dataset to be read in
+    :return: pandas dataframe with contents of SAS dataset
+    """
+    return pd.read_sas(file_path, format="sas7bdat", encoding="iso-8859-1")
+CR = sas_to_dataframe(CT)
+CR.to_csv(CT1)
--- main.py
+++ main.py
@@ -5,12 +5,15 @@ import pandasql as ps
 import numpy as np
 
 count = 0
-COMP_CR = 'C:/Users/Panqiao/Documents/Research/SS - All/COMPSTAT/adsprate.sas7bdat'
-COMP_CRS = 'C:/Users/Panqiao/Documents/Research/SS - All/COMPSTAT/cpstCR.txt'
-COMP_COMP = 'C:/Users/Panqiao/Documents/Research/SS - All/COMPSTAT/COMPSTAT_all_vars_1970_20150301.txt'
+COMP_CR = 'C:/Users/Panqiao/Documents/Research/SS - All/COMPSTAT/adsprate.sas7bdat' #sas Credit rating set
+COMP_CRS = 'C:/Users/Panqiao/Documents/Research/SS - All/COMPSTAT/cpstCR.txt' #text credit rating
+COMP_COMP = 'C:/Users/Panqiao/Documents/Research/SS - All/COMPSTAT/COMPSTAT_all_vars_1970_20150301.txt' # all compustat
 COMP_MCR = 'C:/Users/Panqiao/Documents/Research/SS - All/COMPSTAT/MCR.txt'
 COMP_MCR_FINAL = 'C:/Users/Panqiao/Documents/Research/SS - All/COMPSTAT/MCR_FINAL.txt'
+CRSPLINK  = 'C:/Users/Panqiao/Documents/Research/SS - All/CRSPCOMPSTAT/CRSPCOMPSTATLINK.csv'
 #COMP_CR = 'C:/Users/Panqiao/Documents/Research/SS - All/COMPSTAT/names_adsprate.sas7bdat'
+
+CRPLINK = pd.read_csv(CRSPLINK, sep=",") #collapsed crps compstat link
 #with SAS7BDAT(COMP_CR, skip_header=False) as reader:
    # print(reader[0])
 
@@ -34,9 +37,10 @@ def sas_to_dataframe(file_path):
 #CR = sas_to_dataframe(COMP_CR)
 #CR.to_csv(COMP_CRS)
 
-#CS = pd.read_csv(COMP_COMP, sep="\t", nrows=10000, usecols=[0, 1,2 ,3 ,925 ,1823] )#, header=None)
+CSC = pd.read_csv(COMP_COMP, sep="\t", nrows=10)#, usecols=[0, 1,2 ,3 ,925 ,1823] )#, header=None)
 #CR = pd.read_csv(COMP_CRS, sep=",", nrows=10000)
-CSE = pd.read_csv(COMP_COMP, sep="\t", usecols=[0, 1,2 ,3 ,925 ,1823] )#, header=None)
+#Merge compustat and CRating
+CSE = pd.read_csv(COMP_COMP, sep="\t", usecols=[0, 1, 2, 3, 4, 925, 1823] )#, header=None)
 CS = pd.read_csv(COMP_COMP, sep="\t", usecols=[0, 1] )#, header=None)
 CR = pd.read_csv(COMP_CRS, sep=",")
 #CSS = CS[(CS.gvkey == 1004)]
@@ -45,6 +49,7 @@ print(CR.describe)
 CS.dtypes
 CR.dtypes
 
+
 dates_CR = CR['datadate'].tolist() # turn date into python list
 for x, item in enumerate(dates_CR):
     dates_CR[x] = "".join(item.split("-"))
@@ -52,7 +57,7 @@ for x, item in enumerate(dates_CR):
 CR['mdatadate'] = dates_CR #creates new column with values of dates_CR
 CR.astype({'mdatadate': 'int64'}).dtypes
 CR = CR.rename(columns = {'datadate':'oldate'})
-CR.astype({'mdatadate': 'int64'}).dtypes
+#CR.astype({'mdatadate': 'int64'}).dtypes
 CR = CR.astype({'mdatadate': 'int64'})
 CR.dtypes
 #CSSmerged = pd.merge(CS, CR, left_on=['gvkey','datadate'], right_on = ['gvkey','mdatadate'], how='left')
@@ -96,8 +101,35 @@ newdf1 = ps.sqldf(sqlcode, locals())
 newdf1.to_csv(COMP_MCR_FINAL)
 newdf1.describe
 
+#Finally add merger to CRSPLINK add permco permno to CR
+#first convert LINKDT and LNEKDDT to dates
+CRPLINK.dtypes
+newdf1.dtypes
+CRPLINK.loc[CRPLINK.LINKENDDT == 'E', 'LINKENDDT'] = 20151231 #convert E to date
+CRPLINK = CRPLINK.astype({'LINKENDDT':'int64'})
+#newdf2 = pd.merge(newdf1, CRPLINK, left_on=['gvkey','datadate'], right_on = ['gvkey','mdatadate'], how='left')
 
+sqlcode = '''
+select *
+from newdf1
+left join CRPLINK on newdf1.gvkey = CRPLINK.gvkey
+where (newdf1.datadate >= CRPLINK.LINKDT and newdf1.datadate < CRPLINK.LINKENDDT)
+'''
+newdf2 = ps.sqldf(sqlcode, locals())
+#newdf2 = newdf2[newdf2.columns[0,1,2,3,4,5,7,8,9,10,11,21,22]]
+newdf2 = newdf2.drop(columns=['Unnamed: 0'])
+newdf2 = newdf2.drop(columns=['conm'])
+newdf2 = newdf2.drop(columns=['tic','cusip','CIK'])
+newdf2 = newdf2.drop(columns=['LINKPRIM','LIID',])
+# remove duplicated columns
+#save again
+COMP_MCR_FINAL_A = 'C:/Users/Panqiao/Documents/Research/SS - All/COMPSTAT/MCR_FINAL_A.txt'
+newdf2.to_csv(COMP_MCR_FINAL_A)
 
+newdf2 = newdf2.loc[:,~newdf2.columns.duplicated()] #works
+
+newdf2 = newdf2.drop(columns=['Unnamed: 0','conm','tic','cusip','CIK','LINKPRIM',
+                              'LIID','LINKTYPE'])
 
 
 B.dtypes
